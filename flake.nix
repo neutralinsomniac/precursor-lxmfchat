f48f42fc890cb63fd892c90e3c7bb66167e7edfc
Jeremy O'Brien <neutral@fastmail.com>
2026-06-13 17:04:18 -0400

    add flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "precursor-reticulum dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAll = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAll (pkgs: {
+        default = import ./shell.nix { inherit pkgs; };
+      });
+    };
+}
